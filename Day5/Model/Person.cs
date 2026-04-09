97da9852a22475a2a59a2b1ddf8c69e9051ec52f
zhangxu <ailen1002@163.com>
2026-04-09 11:11:26 +0800

    面向对象基础

---
+++ Day5/Model/Person.cs
@@ -0,0 +1,28 @@
+﻿// /*
+//  * ================================================================================
+//  * @Author       : Andrew
+//  * @Date         : 04月08日 13:04
+//  * @FilePath     : D:\works\MFCProject\RiderProjects\ConsoleApp1\Day5\Model\Person.cs
+//  * @Description  :
+//  * @Copyright    : Copyright 2015 zhang xu, All rights reserved.
+//  * ================================================================================
+//  */
+
+namespace Day5.Model;
+
+public class Person
+{
+    public string Name { get; init; } = "";
+    public string Gender { get; init; } = "";
+    public int Age { get; init; }
+
+    public void SelfIntroduction()
+    {
+        Console.WriteLine($"你好, 我的名字叫{Name}, 我是一个{Gender}性, 我今年{Age}岁。");
+    }
+
+    public void Eating()
+    {
+        Console.WriteLine($"{Name}一天吃了4顿饭。");
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day5.Model;
+
+namespace Day5;
+
+class Program
+{
+    private static void Main()
+    {
+        var people = new Person
+        {
+            Name = "张三",
+            Gender = "男",
+            Age = 18
+        };
+
+        people.SelfIntroduction();
+        people.Eating();
+    }
+}
