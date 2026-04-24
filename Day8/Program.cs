0d78f85156ecd1bec2041974bdf4d42dad4b33b1
zhangxu <ailen1002@163.com>
2026-04-24 13:49:54 +0800

    委托  Lambda 练习

---
+++ Day8/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var r1 = Calc(1,2,(a,b)=>a+b);
+Console.WriteLine(r1);
+
+
+static int Calc(int a, int b, Func<int, int, int> func)
+{
+    return func(a, b);
+}
+
+var list = new List<int>{1,2,3,4,5};
+var even = list.Where(x=>x%2 == 0);
+foreach (var i in even)
+{
+    Console.WriteLine(i);
+}
