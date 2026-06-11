4394c0cfd28f1bde662a89448187eb531a961bb0
zhangxu <ailen1002@163.com>
2026-06-11 15:47:28 +0800

    增加Log 服务文件

---
+++ ModbusApp/Services/Logging/ILogService.cs
@@ -0,0 +1,22 @@
+﻿// /*
+//  * ================================================================================
+//  * @Author       : Andrew
+//  * @Date         : 06月09日 14:06
+//  * @FilePath     : D:\works\MFCProject\RiderProjects\ConsoleApp1\ModbusApp\Services\Logging\ILogService.cs
+//  * @Description  :
+//  * @Copyright    : Copyright 2015 zhang xu, All rights reserved.
+//  * ================================================================================
+//  */
+
+namespace ModbusApp.Services.Logging;
+
+public interface ILogService
+{
+    void Information(string message, params object[] args);
+    void Warning(string message, params object[] args);
+    void Error(string message, params object[] args);
+    void Error(Exception exception, string message, params object[] args);
+    void Debug(string message, params object[] args);
+    void Verbose(string message, params object[] args);
+    IDisposable PushProperty(string name, object value);
+}
---
+++ ModbusApp/Services/Logging/LogService.cs
@@ -0,0 +1,52 @@
+﻿// /*
+//  * ================================================================================
+//  * @Author       : Andrew
+//  * @Date         : 06月09日 14:06
+//  * @FilePath     : D:\works\MFCProject\RiderProjects\ConsoleApp1\ModbusApp\Services\Logging\LogService.cs
+//  * @Description  :
+//  * @Copyright    : Copyright 2015 zhang xu, All rights reserved.
+//  * ================================================================================
+//  */
+
+using Serilog;
+using Serilog.Context;
+
+namespace ModbusApp.Services.Logging;
+
+public class LoggingService(ILogger logger) : ILogService
+{
+    public void Information(string message, params object[] args)
+    {
+        logger.Information(message, args);
+    }
+
+    public void Warning(string message, params object[] args)
+    {
+        logger.Warning(message, args);
+    }
+
+    public void Error(string message, params object[] args)
+    {
+        logger.Error(message, args);
+    }
+
+    public void Error(Exception exception, string message, params object[] args)
+    {
+        logger.Error(exception, message, args);
+    }
+
+    public void Debug(string message, params object[] args)
+    {
+        logger.Debug(message, args);
+    }
+
+    public void Verbose(string message, params object[] args)
+    {
+        logger.Verbose(message, args);
+    }
+
+    public IDisposable PushProperty(string name, object value)
+    {
+        return LogContext.PushProperty(name, value);
+    }
+}
